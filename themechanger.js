ca319356b025366e22562b54cdd41e76b48e8253
Emmanuel <emmanuel@lvl1.fr>
2021-12-08 16:00:06 +0100

    scrollbar

--- themechanger.js
+++ themechanger.js
@@ -1,15 +1,25 @@
-let darkModeButton = document.getElementById('.switchcontainer');
-let icone = document.getElementById('header i');
+let darkModeButton = document.querySelector('.switchcontainer');
+let icone = document.querySelector('header .far');
 
 function darkMode(){
     if(localStorage.getItem('darkMode') === null){
         localStorage.setItem('darkMode', 'enabled');
+        icone.classList.remove('fa-sun');
+        icone.classList.add('fa-moon');
     }
     if(localStorage.getItem('darkMode') === 'enabled'){
         localStorage.setItem('darkMode', 'disabled');
+        icone.classList.remove('fa-moon');
+        icone.classList.add('fa-sun');
     }else{
         localStorage.setItem('darkMode', 'enabled');
+        icone.classList.remove('fa-sun');
+        icone.classList.add('fa-moon');
     }
+}
+
+if(!localStorage.getItem('darkMode')){
+    localStorage.setItem('darkMode', 'disabled');
 }
 
 if(localStorage.getItem('darkMode') === 'enabled'){
