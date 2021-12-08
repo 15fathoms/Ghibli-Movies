187a7bbe44d165fecc7f958bca950315fab272a7
Emmanuel <emmanuel@lvl1.fr>
2021-12-08 16:18:57 +0100

    final commit

--- themechanger.js
+++ themechanger.js
@@ -6,30 +6,41 @@ function darkMode(){
         localStorage.setItem('darkMode', 'enabled');
         icone.classList.remove('fa-sun');
         icone.classList.add('fa-moon');
+        document.body.classList.add('dark');
     }
     if(localStorage.getItem('darkMode') === 'enabled'){
         localStorage.setItem('darkMode', 'disabled');
         icone.classList.remove('fa-moon');
         icone.classList.add('fa-sun');
+        document.body.classList.remove('dark');
     }else{
         localStorage.setItem('darkMode', 'enabled');
         icone.classList.remove('fa-sun');
         icone.classList.add('fa-moon');
+        document.body.classList.add('dark');
     }
 }
 
 if(!localStorage.getItem('darkMode')){
     localStorage.setItem('darkMode', 'disabled');
 }
-
 if(localStorage.getItem('darkMode') === 'enabled'){
     icone.classList.add('fa-moon');
+    if(!document.body.classList.contains('dark')){
+        document.body.classList.add('dark');
+    }
 }
 else if(localStorage.getItem('darkMode') === 'disabled'){
     icone.classList.add('fa-sun');
+    if(document.body.classList.contains('dark')){
+        document.body.classList.remove('dark');
+    }
 }
 else if(localStorage.getItem('darkMode') === null){
     icone.classList.add('fa-sun');
+    if(document.body.classList.contains('dark')){
+        document.body.classList.remove('dark');
+    }
 }
 
 darkModeButton.addEventListener('click', darkMode);
